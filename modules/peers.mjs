7db1de1907ed6595ab616036b3c7a83749ee14c4
MyEcoria <github@myecoria.com>
2023-07-28 19:21:58 +0200

    add secure

--- modules/peers.mjs
+++ modules/peers.mjs
@@ -3,6 +3,7 @@ import DHT from 'bittorrent-dht';
 import blake from 'blakejs';
 import cool from 'magnet-uri';
 import geoip from 'geoip-lite';
+import net from 'net';
 
 export function getRandomNumberBetween(min, max) {
   // Generate a random number between 0 (inclusive) and 1 (exclusive)
@@ -17,31 +18,64 @@ export function getRandomNumberBetween(min, max) {
   return randomNumber;
 }
 
-export default async function bittorrentDHT(id, magnet) {
-  const port = getRandomNumberBetween(2000, 6000);
-  const dht = new DHT();
-  dht.listen(port, function () {
-    console.log(`DHT listening on port ${port}`);
+async function isPortAvailable(port) {
+  return new Promise((resolve) => {
+    const server = net.createServer();
+    server.on('error', () => {
+      resolve(false);
+    });
+    server.listen(port, () => {
+      server.close(() => {
+        resolve(true);
+      });
+    });
   });
+}
 
-  dht.on('peer', async function (peer, infoHash, from) {
-    try {
-      var geo = await geoip.lookup(peer.host);
-      //console.log(peer.host);
-      let country;
-      if (geo && geo.country) {
-        country = geo.country;
-      } else {
-        country = "nop";
-      }
+export async function getRandomAvailablePort(min, max) {
+  const maxAttempts = 10;
+  let attempts = 0;
 
-      await db.createPeer(peer.host, id, blake.blake2bHex(`${peer.host}for${magnet}`), country);
-    } catch (error) {
-      console.error('Error looking up geo information:', error.message);
+  while (attempts < maxAttempts) {
+    const port = getRandomNumberBetween(min, max);
+    const isAvailable = await isPortAvailable(port);
+    if (isAvailable) {
+      return port;
     }
-  });
+    attempts++;
+  }
 
-  // find peers for the given torrent info hash
-  const parsed = cool(magnet);
-  dht.lookup(parsed.infoHash);
+  throw new Error(`Failed to find an available port after ${maxAttempts} attempts.`);
+}
+
+export default async function bittorrentDHT(id, magnet) {
+  try {
+    const port = await getRandomAvailablePort(1, 65535);
+    const dht = new DHT();
+    dht.listen(port, function () {
+      console.log(`DHT listening on port ${port}`);
+    });
+
+    dht.on('peer', async function (peer, infoHash, from) {
+      try {
+        var geo = await geoip.lookup(peer.host);
+        let country;
+        if (geo && geo.country) {
+          country = geo.country;
+        } else {
+          country = "nop";
+        }
+
+        await db.createPeer(peer.host, id, blake.blake2bHex(`${peer.host}for${magnet}`), country);
+      } catch (error) {
+        console.error('Error looking up geo information:', error.message);
+      }
+    });
+
+    // find peers for the given torrent info hash
+    const parsed = cool(magnet);
+    dht.lookup(parsed.infoHash);
+  } catch (error) {
+    console.error('Error creating DHT:', error.message);
+  }
 }
